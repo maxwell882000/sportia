aff28f2cc24c40e26a0908a4d1dd71ccc3627c85
shokhzodmurodov <shaxa882@gmail.com>
2024-12-20 14:23:35 +0500

    fixed showing login page

--- src/states/handler.ts
+++ src/states/handler.ts
@@ -1,12 +1,14 @@
 import { AuthError } from "../infrastructure/axios/exceptions/authError.ts";
 import { $userPopUpChanged } from "./profile/events.ts";
 import { UserPopUp } from "../dtos/users/userPopUp.ts";
+import { AuthStorage } from "../infrastructure/localStorage/authStorage.ts";
 
 export async function requestHandler<T>(request: Promise<any>): Promise<T> {
   try {
     return await request;
   } catch (e) {
     if (e instanceof AuthError) {
+      AuthStorage.deleteToken();
       $userPopUpChanged(UserPopUp.LOGIN);
     }
     console.error(e);
